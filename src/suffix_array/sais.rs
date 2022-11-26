e58bcae027cc83eb7dcf99a37d149b6d998a9638
Pascal Mehnert <pascalmehnert@posteo.de>
2022-11-26 18:48:19 +0100

    Clean up code for induction buckets

--- src/suffix_array/sais.rs
+++ src/suffix_array/sais.rs
@@ -1,12 +1,10 @@
-use std::iter::{once, Enumerate, Peekable, Rev};
-use std::ops::Deref;
-use std::{mem, slice};
+use std::iter::{Enumerate, Peekable, Rev};
+use std::slice;
 
+use self::buckets::Buckets;
 use super::SuffixArray;
 use crate::TextExt;
 
-// todo replace with const generic on Buckets
-pub(super) const ALPHABET_SIZE: usize = u8::MAX as usize + 1;
 
 pub(super) fn construct(text: &[u8]) -> SuffixArray {
     let mut sa = vec![0; text.len()];
@@ -20,7 +18,7 @@ pub(super) fn construct(text: &[u8]) -> SuffixArray {
     // put sorted LMS-suffixes at the end of buckets
     // TODO this is ugly as the night
     let mut lms_iter = lms.iter().rev().peekable();
-    for (symbol, bucket_end) in buckets.end.iter().enumerate().rev() {
+    for (symbol, bucket_end) in buckets.end().iter().enumerate().rev() {
         let mut idx = *bucket_end;
         'inner: while let Some(&&i) = lms_iter.peek() {
             if text[i] != symbol as u8 {
@@ -43,18 +41,19 @@ pub(super) fn construct(text: &[u8]) -> SuffixArray {
 fn induce_l_suffixes(text: &[u8], sa: &mut [usize], buckets: &mut Buckets) {
     assert_eq!(text.len(), sa.len());
 
+    // Emulate S* suffix of guardian element
     if let Some(last) = text.last() {
-        sa[buckets.take_l_index(*last)] = sa.len() - 1;
+        sa[buckets.get_mut(*last).take_first()] = sa.len() - 1;
     }
 
     for i in 0..sa.len() {
         if sa[i] != 0 {
             let is_l_type = text[sa[i] - 1] > text[sa[i]]
                 || (text[sa[i] - 1] == text[sa[i]]
-                    && i < buckets.begin[text[sa[i]] as usize]);
+                    && i < buckets.get(text[sa[i]]).begin());
 
             if is_l_type {
-                sa[buckets.take_l_index(text[sa[i] - 1])] = sa[i] - 1;
+                sa[buckets.get_mut(text[sa[i] - 1]).take_first()] = sa[i] - 1;
             }
         }
     }
@@ -67,86 +66,15 @@ fn induce_s_suffixes(text: &[u8], sa: &mut [usize], buckets: &mut Buckets) {
         if sa[i] != 0 {
             let is_s_type = text[sa[i] - 1] < text[sa[i]]
                 || (text[sa[i] - 1] == text[sa[i]]
-                    && i >= buckets.begin[text[sa[i]] as usize]);
+                    && i >= buckets.get(text[sa[i]]).begin());
 
             if is_s_type {
-                sa[buckets.take_s_index(text[sa[i] - 1])] = sa[i] - 1;
+                // TODO bucket_end is never actually read again ...
+                sa[buckets.get_mut(text[sa[i] - 1]).take_last()] = sa[i] - 1;
             }
         }
     }
 }
-
-struct Buckets {
-    begin: [usize; ALPHABET_SIZE],
-    end: [usize; ALPHABET_SIZE],
-}
-
-impl Buckets {
-    pub fn new(text: &[u8]) -> Self {
-        let mut histogram = [0_usize; ALPHABET_SIZE];
-        for c in text {
-            histogram[*c as usize] += 1;
-        }
-
-        let sum = &mut 0;
-        let bucket_start = histogram.map(|n| mem::replace(sum, *sum + n));
-
-        // TODO reuse histogram here
-        let mut sum = 0;
-        let bucket_end = histogram.map(|n| {
-            sum += n;
-            sum
-        });
-
-        Buckets { begin: bucket_start, end: bucket_end }
-    }
-
-    pub fn take_l_index(&mut self, symbol: u8) -> usize {
-        let idx = &mut self.begin[symbol as usize];
-        mem::replace(idx, *idx + 1)
-    }
-
-    pub fn take_s_index(&mut self, symbol: u8) -> usize {
-        let idx = &mut self.end[symbol as usize];
-        *idx -= 1;
-        *idx
-    }
-}
-
-// TODO would this make sense?
-impl Buckets {
-    fn bucket(&self, symbol: u8) -> Bucket<&usize> {
-        Bucket {
-            begin: &self.begin[symbol as usize],
-            end: &self.end[symbol as usize],
-        }
-    }
-
-    fn bucket_mut(&mut self, symbol: u8) -> Bucket<&mut usize> {
-        Bucket {
-            begin: &mut self.begin[symbol as usize],
-            end: &mut self.end[symbol as usize],
-        }
-    }
-}
-
-struct Bucket<T> {
-    begin: T,
-    end: T,
-}
-
-impl<T: Deref<Target = usize>> Bucket<T> {
-    fn begin(&self) -> usize { *self.begin }
-
-    fn end(&self) -> usize { *self.end }
-}
-
-impl Bucket<&mut usize> {
-    fn take_first(&mut self) -> usize { todo!() }
-
-    fn take_last(&mut self) -> usize { todo!() }
-}
-
 
 struct LMSIter<'a> {
     text: Peekable<Rev<Enumerate<slice::Iter<'a, u8>>>>,
@@ -168,7 +96,6 @@ impl<'a> Iterator for LMSIter<'a> {
     fn next(&mut self) -> Option<Self::Item> {
         let mut curr = self.text.next()?;
 
-        // TODO is it ok to advance twice right away?
         while let Some(&next) = self.text.peek() {
             if self.is_s_suffix {
                 if next.1 > curr.1 {
@@ -178,12 +105,88 @@ impl<'a> Iterator for LMSIter<'a> {
             } else {
                 self.is_s_suffix = next.1 < curr.1;
             }
-            let _ = self.text.next();
+            self.text.next();
             curr = next;
         }
         None
     }
 }
+
+#[allow(dead_code)]
+mod buckets {
+    use std::{mem, ops::Deref};
+
+    #[must_use]
+    pub(super) struct Buckets {
+        begin: [usize; Self::SIZE],
+        end: [usize; Self::SIZE],
+    }
+
+    impl Buckets {
+        pub const SIZE: usize = u8::MAX as usize + 1;
+
+        pub fn new(text: &[u8]) -> Self {
+            let mut histogram = [0_usize; Self::SIZE];
+            for c in text {
+                histogram[*c as usize] += 1;
+            }
+
+            let sum = &mut 0;
+            let bucket_start = histogram.map(|n| mem::replace(sum, *sum + n));
+
+            // TODO reuse histogram here
+            let mut sum = 0;
+            let bucket_end = histogram.map(|n| {
+                sum += n;
+                sum
+            });
+
+            Buckets { begin: bucket_start, end: bucket_end }
+        }
+
+        pub fn begin(&self) -> &[usize; Self::SIZE] { &self.begin }
+
+        pub fn end(&self) -> &[usize; Self::SIZE] { &self.end }
+
+        pub fn get(&self, symbol: u8) -> Bucket<&usize> {
+            Bucket {
+                begin: &self.begin[symbol as usize],
+                end: &self.end[symbol as usize],
+            }
+        }
+
+        pub fn get_mut(&mut self, symbol: u8) -> Bucket<&mut usize> {
+            Bucket {
+                begin: &mut self.begin[symbol as usize],
+                end: &mut self.end[symbol as usize],
+            }
+        }
+    }
+
+    #[must_use]
+    pub(super) struct Bucket<T> {
+        begin: T,
+        end: T,
+    }
+
+    impl<T: Deref<Target = usize>> Bucket<T> {
+        pub fn begin(&self) -> usize { *self.begin }
+
+        pub fn end(&self) -> usize { *self.end }
+    }
+
+    impl Bucket<&mut usize> {
+        pub fn take_first(&mut self) -> usize {
+            mem::replace(self.begin, *self.begin + 1)
+        }
+
+        pub fn take_last(&mut self) -> usize {
+            *self.end -= 1;
+            *self.end
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod test {
